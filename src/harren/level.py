bff52682b61abf4292c33740e1d738eebb32fc36
Brant Watson <brant.watson@propylon.com>
2018-11-20 17:43:50 -0600

    A bit more work on the level state

--- src/harren/level.py
+++ src/harren/level.py
@@ -12,6 +12,22 @@ LOG = logging.getLogger(__name__)
 
 class Level(object):
 
-    def __init__(self, filename, **kwargs):
+    def __init__(self, filename, screen, **kwargs):
         map_path = os.path.join(resources.TMX_FOLDER, filename)
+        self.screen = screen
         self.tile_renderer = TileRenderer(map_path)
+
+    def draw(self):
+        map_image = self.tile_renderer.make_2x_map()
+        map_rect = map_image.get_rect()
+        viewport = self.screen.get_rect(bottom=map_rect.bottom)
+        surface = pg.Surface((map_rect.width, map_rect.height)).convert()
+
+        # Draw map first
+        surface.blit(map_image, viewport, viewport)
+
+        # Next maybe the player(s)
+        # NOT IMPLEMENTED
+
+        # Finally any dialog
+        # NOT IMPLEMENTED
