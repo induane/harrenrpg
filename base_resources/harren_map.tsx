b5a5364c3435494a11aa8307f75903c3f5b82dc4
Brant Watson <brant.watson@propylon.com>
2018-11-25 23:50:51 +0000

    Add harren_overworld

--- base_resources/harren_map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="harren_map" tilewidth="32" tileheight="32" tilecount="5146" columns="83">
- <image source="../graphics/MapCurrent_block32.png" width="2667" height="2000"/>
-</tileset>
