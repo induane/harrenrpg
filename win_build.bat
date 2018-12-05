5fd38417700566b6f3d7dd6ea69f1f915221e3f5
Brant Watson <brant.watson@propylon.com>
2018-12-05 15:21:11 -0600

    update spec

--- win_build.bat
+++ win_build.bat
@@ -1,1 +1,1 @@
-pyinstaller src\harren\entry_point.py --hidden-import=pygame --hidden-import=log-color --hidden-import=six --hidden-import=toml --hidden-import=boltons -p src\harren --add-data=src\harren;harren --name=harren --onefile
+pyinstaller src\harren\entry_point.py --hidden-import=pygame --hidden-import=log-color --hidden-import=six --hidden-import=toml --hidden-import=boltons -p src\harren --add-data=src\harren;harren --name=harren --onefile --noconsole
